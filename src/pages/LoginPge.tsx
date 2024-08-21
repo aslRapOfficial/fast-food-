40240c6bbe11bc6ce9726a1aee7bd8727ef9ace2
ElmurodYoldoshev <eyoldoshev551@gmail.com>
2024-08-21 11:57:04 +0500

    edit test

---
+++ src/pages/LoginPge.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+// import login from "../assets/login.png";
+
+const LoginPge: React.FC = () => {
+  return (
+    <div>
+      <div>
+        {/* <img src={login} alt="Login" /> */}
+      </div>
+      <div>
+        <div>
+          <h1></h1>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default LoginPge;
